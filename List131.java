73040e0e16453668f4dff50f508d4c87f6207195
hiragi <sasanoha31@gmail.com>
2011-10-01 12:23:29 +0900

    station

---
+++ List131.java
@@ -0,0 +1,64 @@
+// プログラムコードによる駅の路線図
+import java.util.Vector;
+
+public class List131{
+	private static class Station{
+		public String name;
+		public Vector transitions;
+
+		public Station(String name){
+			this.name = name;
+			this.transitions = new Vector();
+		}
+
+		public void addStation(Station transition){
+			for(int i=0; i<this.transitions.size(); i++){
+				if(((Station) this.transitions.get(i)).name.equals(transition.name)){
+					// すでに登録されているので、何もせずに終了する
+					return;
+				}
+			}
+			// 新しい乗換駅を登録する
+			this.transitions.add(transition);
+		}
+
+		public void printStation(){
+			System.out.print(this.name + ":");
+			for(int i=0; i<this.transitions.size(); i++){
+				System.out.print("→" + ((Station) this.transitions.get(i)).name + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String args[]){
+		Station[] station = new Station[6];
+		station[0] = new Station("鎌倉");
+		station[1] = new Station("藤沢");
+		station[2] = new Station("横浜");
+		station[3] = new Station("横須賀");
+		station[4] = new Station("茅ヶ崎");
+		station[5] = new Station("東京");
+		
+		station[0].addStation(station[3]);
+		station[0].addStation(station[1]);
+		station[0].addStation(station[2]);
+
+		station[1].addStation(station[0]);
+		station[1].addStation(station[4]);
+		station[1].addStation(station[2]);
+
+		station[2].addStation(station[1]);
+		station[2].addStation(station[0]);
+		station[2].addStation(station[5]);
+
+		station[3].addStation(station[0]);
+		station[4].addStation(station[1]);
+		station[5].addStation(station[2]);
+
+		for(int i=0; i<6; i++){
+			station[i].printStation();
+		}
+	}
+}
+
